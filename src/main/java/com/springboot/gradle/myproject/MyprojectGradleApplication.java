4a307a1260c8439b961b0afd4e6973ad67eee22c
Terry2017 <terry2015zhb@163.com>
2017-01-08 15:42:46 +0800

    my project

---
+++ src/main/java/com/springboot/gradle/myproject/MyprojectGradleApplication.java
@@ -0,0 +1,12 @@
+package com.springboot.gradle.myproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyprojectGradleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyprojectGradleApplication.class, args);
+	}
+}
